b1522a32ad908468ec89b1b84ca103911cdaa769
isitheh@gmail.com <isitheh@gmail.com>
2024-03-07 12:47:04 +0200

    Part 3: Create a feature component.

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -3,13 +3,14 @@ import { Hero } from '../hero';
 import { CommonModule, NgIf, NgFor, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  imports: [CommonModule, FormsModule, NgFor, NgIf, UpperCasePipe],
+  imports: [CommonModule, FormsModule, NgFor, NgIf, UpperCasePipe, HeroDetailComponent],
 })
 
 export class HeroesComponent {
