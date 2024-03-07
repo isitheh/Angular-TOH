4389d87ffba0dc4775a62b5afa175049c9c92655
isitheh@gmail.com <isitheh@gmail.com>
2024-03-07 05:56:37 +0200

    Created the Hero Editor.

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Testing - angular-tour-of-heroes';
+  title = 'Angular Tour of Heroes Tutorial';
 }
